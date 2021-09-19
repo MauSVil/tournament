ff6c9e059a3f22b9045ed9a5ed32229827ba386e
MauSVil <maudev96@gmail.com>
2021-09-19 02:17:21 -0500

    production

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 import envJSON from './env.variables.json';
 
-const node_env = process.env.NODE_ENV || 'development';
+const node_env = process.env.NODE_ENV || 'production';
 console.log(process.env, 'processEnv')
 const envVariables = envJSON[node_env];
 
