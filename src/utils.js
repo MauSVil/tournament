3a837cfa948102162b9d44e35c643956de1b8442
MauSVil <maudev96@gmail.com>
2021-09-19 02:37:15 -0500

    process env fix

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,6 @@
 import envJSON from './env.variables.json';
 
-const node_env = 'production';
-console.log(process.env, 'processEnv')
+const node_env = process.env.REACT_APP_NODE_ENV || 'development';
 const envVariables = envJSON[node_env];
 
 export default envVariables;
