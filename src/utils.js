abc93973930f43bea7832e61fe92f1f584a9ae0e
MauSVil <maudev96@gmail.com>
2021-09-19 02:01:10 -0500

    process ENV log

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 import envJSON from './env.variables.json';
 
 const node_env = process.env.NODE_ENV || 'development';
+console.log(process.env, 'processEnv')
 const envVariables = envJSON[node_env];
 
 export default envVariables;
